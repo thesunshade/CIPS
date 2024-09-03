033aafbb7ce81cf4c320c40552266d3b688c2a3c
thesunshade <82448383+thesunshade@users.noreply.github.com>
2024-09-03 21:06:40 +1200

    ignore dash in instant search

--- src/functions/scripts.js
+++ src/functions/scripts.js
@@ -19,7 +19,7 @@ function normalizeString(str) {
   return str
     .normalize("NFD") // Decompose diacritics
     .replace(/[\u0300-\u036f]/g, "") // Remove diacritics
-    .replace(/[\s,;.“”'"’/()]/g, "") // Remove spaces and punctuation
+    .replace(/[\s,;.“”'"’/()-]/g, "") // Remove spaces and punctuation
     .toLowerCase();
 }
 
