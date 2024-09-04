329cd37a4cf7b819764e166e835641ff7eef8311
thesunshade <82448383+thesunshade@users.noreply.github.com>
2024-09-04 14:09:30 +1200

    don't bundle

--- src/functions/createSuttaIndexHtml.js
+++ src/functions/createSuttaIndexHtml.js
@@ -185,8 +185,10 @@ export default function createSuttaIndexHtml(indexObject) {
   const bundledCssFile = "public/index.css";
   const mini = "";
   // const mini ="--minify"
+  const bundleJs = "";
+  // const bundleJs ="--bundle"
   //
-  exec(`npx esbuild ${rawJsFile} --bundle ${mini} --outfile=${bundledJsFile}`, (error, stdout, stderr) => {
+  exec(`npx esbuild ${rawJsFile} ${bundleJs} ${mini} --outfile=${bundledJsFile}`, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error: ${error.message}`);
       return;
