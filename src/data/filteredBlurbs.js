509a05516c396a04840ffe7753f901c9511af6d2
thesunshade <82448383+thesunshade@users.noreply.github.com>
2025-01-22 19:53:07 +1300

    updates (DN9)

--- src/data/filteredBlurbs.js
+++ src/data/filteredBlurbs.js
@@ -3270,5 +3270,6 @@ export const filteredBlurbs ={
      "dn5": "A brahmin wishes to undertake a great sacrifice, and asks for the Buddha’s advice. The Buddha tells a legend of the past, in which a king is persuaded to give up violent sacrifice, and instead to devote his resources to supporting the needy citizens of his realm. However, even such a beneficial and non-violent sacrifice pales in comparison to the spiritual sacrifice of giving up attachments.",
      "dn6": "The Buddha explains to a diverse group of lay people how the results of meditation depend on the manner of development.",
      "dn7": "This discourse is mostly quoted by the Buddha in the previous.",
-     "dn8": "The Buddha is challenged by a naked ascetic on the topic of spiritual austerities. He points out that it is quite possible to perform all kinds of austere practices without having any inner purity of mind."
+     "dn8": "The Buddha is challenged by a naked ascetic on the topic of spiritual austerities. He points out that it is quite possible to perform all kinds of austere practices without having any inner purity of mind.",
+     "dn9": "The Buddha discusses with a wanderer the nature of perception and how it evolves through deeper states of meditation. None of these, however, should be identified with a self or soul."
 }
