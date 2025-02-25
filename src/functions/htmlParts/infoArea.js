ddb50c62f3ba5576a341a3059c43a1c9a6b79b23
thesunshade <82448383+thesunshade@users.noreply.github.com>
2025-02-25 20:51:47 +1300

    updates DN26

--- src/functions/htmlParts/infoArea.js
+++ src/functions/htmlParts/infoArea.js
@@ -68,7 +68,7 @@ export const infoArea = `
         <summary>Coverage</summary>
         <div class="row-2">
           <div>
-            Complete: <strong>DN 1–21, MN, SN, AN, Kp, Dhp, Ud, Iti, Snp, Vv, Pv, Thag, Thig</strong>
+            Complete: <strong>DN 1–26, MN, SN, AN, Kp, Dhp, Ud, Iti, Snp, Vv, Pv, Thag, Thig</strong>
           </div>
             <div class="stats">
             <div><em>Stats:</em></div>
